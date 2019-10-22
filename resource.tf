f965e3cd3382a635b6d65be2e1053549116bec45
bhargav <35688223+9492678901@users.noreply.github.com>
2019-10-22 13:50:57 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "apache_webserver_1"{
-	ami= "ami-00c03f7f7f2ec15c3"
+	ami= "ami-02913db388613c3e1"
 	instance_type= "t2.micro"
 	user_data= "${file("serverInstallAndDeploy.sh")}"
 	key_name="test"
@@ -8,7 +8,7 @@ resource "aws_instance" "apache_webserver_1"{
   	}
 }
 resource "aws_instance" "apache_webserver_2"{
-        ami= "ami-00c03f7f7f2ec15c3"
+        ami= "ami-02913db388613c3e1"
         instance_type= "t2.micro"
 	user_data= "${file("serverInstallAndDeploy.sh")}"
 	key_name="test"
@@ -18,7 +18,7 @@ resource "aws_instance" "apache_webserver_2"{
 }
 resource "aws_instance" "nginxloadbalancer"{
         #ami= "ami-0270f291a8a0f0d6b"
-	ami="ami-00c03f7f7f2ec15c3"
+	ami="ami-02913db388613c3e1"
         instance_type= "t2.micro"
 	key_name="test"
         user_data = "${data.template_file.init.rendered}"
